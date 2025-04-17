5c12bf5e422d282b9f48b858155ca02cfac21c04
lucas45624 <luc.felipe50@gmail.com>
2025-04-17 16:38:13 -0300

    Add files via upload

---
+++ repetição 1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Digite números inteiros (digite um número negativo para sair):\n");
+
+    while (1) {
+        printf("Digite um número: ");
+        scanf("%d", &numero);
+
+        if (numero < 0) {
+            printf("Número negativo digitado. Encerrando o programa.\n");
+            break; // Sai do loop
+        }
+
+        printf("Você digitou: %d\n", numero);
+    }
+
+    return 0;}
+
